3d343d8c9799c1be3c9b8f8000632530a8931f7e
Mohamed Abdallah <MAbdallah@integrant.com>
2017-08-07 00:28:58 +0200

    Commit 2 - Branch 1

--- Code/Code/Program.cs
+++ Code/Code/Program.cs
@@ -13,6 +13,7 @@ namespace Code
             Console.WriteLine("Commit 1");
             Console.WriteLine("Commit 2");
             Console.WriteLine("Commit 1 - Branch 1");
+            Console.WriteLine("Commit 2 - Branch 1");
         }
     }
 }
